7282d4d8e06c1553e2fe6493dc1db84997622e08
amcamd <andrew.chapman@amd.com>
2020-03-23 15:14:56 -0400

    copy to directory not file

--- Tensile/Source/TensileCreateLibrary.cmake
+++ Tensile/Source/TensileCreateLibrary.cmake
@@ -55,7 +55,7 @@ function(TensileCreateLibraryCmake
 
   file (RENAME ${Tensile_ROOT}/bin/TensileCreateLibrary ${Tensile_ROOT}/bin/TensileCreateLibrary.tmp)
 
-  file (COPY ${Tensile_ROOT}/bin/TensileCreateLibrary.tmp DESTINATION ${Tensile_ROOT}/bin/TensileCreateLibrary FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_WRITE GROUP_EXECUTE WORLD_READ WORLD_WRITE WORLD_EXECUTE)
+  file (COPY ${Tensile_ROOT}/bin/TensileCreateLibrary.tmp DESTINATION ${Tensile_ROOT}/bin FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_WRITE GROUP_EXECUTE WORLD_READ WORLD_WRITE WORLD_EXECUTE)
 
 
   set(Tensile_SOURCE_PATH "${PROJECT_BINARY_DIR}/Tensile")
